7a5953e2574e707dfe5f863205dda4ce6cdb8aee
sanicboi <alexandrzezekalo@gmail.com>
2025-01-09 13:21:59 +0000

    New input

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,8 @@
   <h3 class="text-neutral-900 text-4xl font-normal text-center ">Main text</h3>
   <h3 class="text-neutral-50 text-4xl text-center  bg-orange-400">Main text 2</h3>
   <h3 class="text-neutral-50 text-4xl text-center  bg-lime-400">Main text 3</h3>
-  <button class="text-neutral-50 text-2xl text-center font-medium px-4 py-2 leading-none rounded-md bg-lime-400">Button 1</button>
-  <button class="text-neutral-50 text-xl text-center px-2 py-1 leading-none rounded-md hover:bg-red-50 hover:text-red-500 transition-all duration-500 bg-red-500  ">Button 2</button>
-  <input type="text" placeholder="input" class="appearance-none montserrat outline-none text-neutral-50 text-xl px-2 py-2 self-stretch mx-16 placeholder-red-50 focus-visible:placeholder-neutral-600 focus-visible:text-neutral-900 leading-none rounded-md focus-visible:bg-red-50  transition-all duration-500 bg-red-500">
-  <input type="text" placeholder="Input" class="appearance-none montserrat outline-none font-medium text-xl px-2 py-2 self-stretch mx-16 text-neutral-900 placeholder:text-neutral-400 leading-none rounded-md bg-red-50 border-red-500 border-2">
-
+  <input type="text" placeholder="Input" class="appearance-none outline-none px-2 py-2 bg-neutral-200 rounded-lg border-2 placeholder:text-neutral-600 focus-within:placeholder:text-neutral-600 placeholder:font-normal  text-neutral-950 font-semibold">
+  <button class="">Submit</button>
 </div>
 </template>
 
