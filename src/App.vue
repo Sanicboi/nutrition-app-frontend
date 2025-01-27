ca4c3f7c5f4eec6df8f15e7aae68384efce92ed0
sanicboi <alexandrzezekalo@gmail.com>
2025-01-27 14:54:45 +0000

    first page started

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
+
 
 
 
@@ -7,16 +9,28 @@
 </script>
 
 <template>
-<div class="flex flex-col gap-y-8 items-center justify-start">
-  <h1 class="text-orange-400 text-4xl font-bold text-center">Primary heading 1</h1>
-  <h2 class="text-lime-400 text-4xl font-medium text-center ">Secondary heading 1</h2>
-  <h3 class="text-neutral-900 text-4xl font-normal text-center ">Main text</h3>
-  <h3 class="text-neutral-50 text-4xl text-center  bg-orange-400">Main text 2</h3>
-  <h3 class="text-neutral-50 text-4xl text-center  bg-lime-400">Main text 3</h3>
-  <input type="text" placeholder="Input" class="appearance-none outline-none px-2 py-2 bg-neutral-200 rounded-lg border-2 placeholder:text-neutral-600 focus-within:placeholder:text-neutral-600 placeholder:font-normal  text-neutral-950 font-semibold">
-  <button class="">Submit</button>
-</div>
+  <main class="flex flex-col justify-stretch items-stretch min-h-screen">
+    <RouterView></RouterView>
+  </main>
+  <nav class="sticky bottom-0 h-10 py-8 px-8 flex justify-between align-center bg-neutral-200 font-semibold text-xl">
+    <button class="btn">
+      <i class="ri-run-line"></i>
+      <p class="">Training</p>
+    </button>
+    <button class="btn">
+      <i class="ri-restaurant-line"></i>
+      <p class="">Nutrition</p>
+    </button>
+    <button class="btn">
+      <i class="ri-user-line"></i>
+      <p class="">Profile</p>
+    </button>
+  </nav>
+
 </template>
 
 <style scoped>
+.btn {
+  @apply text-center leading-none flex flex-col items-center justify-center;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'remixicon/fonts/remixicon.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
